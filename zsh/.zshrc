afa6430b7d97fe704e713bdd5126976a8aec60a5
Edlison <bolin.edlison.shen@gmail.com>
2023-09-11 15:03:02 -0400

    init repo

---
+++ zsh/.zshrc
@@ -0,0 +1,7 @@
+# The only use of this file is to import modules
+
+export DOTFILES=$HOME/.dotfiles
+source $DOTFILES/zsh/env.zsh
+source $DOTFILES/zsh/var.zsh
+source $DOTFILES/zsh/alias.zsh
+source $DOTFILES/zsh/settings.zsh
---
+++ zsh/alias.zsh
@@ -0,0 +1,9 @@
+# Alias
+alias cl='clear'
+alias vim='nvim'
+alias t='tmux'
+alias zshconfig="vim $DOTFILES/zsh/settings.zsh"
+
+## Hexo
+alias blogg="cd ~/Documents/Code/GitHub/blog && hexo g"
+alias blogd="cd ~/Documents/Code/GitHub/blog && hexo d"
