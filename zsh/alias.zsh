d20ffd663ccb7e8b5383c21a9443a3bc67107c1e
Edlison <bolin.edlison.shen@gmail.com>
2023-09-11 18:53:44 -0400

    update git, update zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,7 +1,9 @@
 # Alias
 alias cl='clear'
 alias vim='nvim'
-alias zshconfig="vim $DOTFILES/zsh/settings.zsh"
+alias zshconfig='vim $DOTFILES/zsh/settings.zsh'
+alias zshalias='vim $DOTFILES/zsh/alias.zsh'
+alias zshenvs='vim $DOTFILES/zsh/envs.zsh'
 
 ## tmux
 alias td='tmux detach'
